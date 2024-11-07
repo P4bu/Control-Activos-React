3d483c3c0f31e9cd771d197eab284dda1e0abe29
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-11-07 13:51:36 -0300

    PopUp: se agrega popUp a la funcion agregar salida

--- src/components/ActivoList.tsx
+++ src/components/ActivoList.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Activo, agregarSalida, eliminarActivo } from '../services/activoService';
-import { Edit, Trash, Plus, Ticket } from 'lucide-react';
+import { Edit, Trash, Plus } from 'lucide-react';
+import '../styles/ActivoList.css'; // Asegúrate de crear este archivo y agregar las clases CSS necesarias
 
 interface ActivoListProps {
   activos: Activo[];
@@ -8,14 +9,48 @@ interface ActivoListProps {
 }
 
 const ActivoList: React.FC<ActivoListProps> = ({ activos, setActivos }) => {
+  // Estado para manejar la visibilidad del popup
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // Estado para manejar los datos del formulario del popup
+  const [formData, setFormData] = useState({
+    asignadoA: '',
+    ticket: `TICKET-${Date.now()}`, // Ticket auto-generado basado en timestamp
+    fechaSalida: new Date().toISOString().split('T')[0], // Fecha actual
+  });
+  // Estado para saber qué activo estamos editando
+  const [activoSeleccionado, setActivoSeleccionado] = useState<number | null>(null);
+
   const handleEliminar = (indice: number) => {
     const nuevosActivos = eliminarActivo(activos, indice);
     setActivos(nuevosActivos);
-  }
-  const handleAgregarSalida = (activos: Activo[] ,indice: number, fechaSalida: string, ticket: string, asignadoA: string) => {
-    const nuevosActivos = agregarSalida(activos, indice, fechaSalida, ticket, asignadoA)
-    setActivos(nuevosActivos)
-  }
+  };
+
+  // Función para abrir el modal
+  const handleAgregarSalida = (indice: number) => {
+    setActivoSeleccionado(indice);
+    setIsModalOpen(true);
+    // Restablecer los valores del formulario
+    setFormData({
+      asignadoA: '',
+      ticket: `TICKET-${Date.now()}`, // Nuevo ticket generado
+      fechaSalida: new Date().toISOString().split('T')[0], // Fecha actual
+    });
+  };
+
+  // Función para cerrar el modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Función para manejar la presentación de datos y actualización del activo
+  const handleSubmit = () => {
+    if (activoSeleccionado !== null) {
+      const nuevosActivos = agregarSalida(activos, activoSeleccionado, formData.fechaSalida, formData.ticket, formData.asignadoA);
+      setActivos(nuevosActivos);
+    }
+    closeModal(); // Cerrar el modal después de agregar la salida
+  };
+
   return (
     <div className="mt-4">
       <h2 className="text-2xl font-bold mb-2">Activos</h2>
@@ -28,7 +63,15 @@ const ActivoList: React.FC<ActivoListProps> = ({ activos, setActivos }) => {
                 <p className="text-gray-700">Serie: {activo.serie}</p>
                 <p className="text-gray-700">Orden de Compra: {activo.ordenCompra}</p>
                 <p className="text-gray-700">Fecha de Ingreso: {activo.fechaIngreso}</p>
-                <p className="text-gray-700">Fecha de Salida: {activo.fechaSalida}</p>
+                {activo.asignadoA && (  
+                  <p className="text-gray-700">Asignado: {activo.asignadoA}</p>
+                )}
+                {activo.fechaSalida && (
+                  <p className="text-gray-700">Fecha de Salida: {activo.fechaSalida}</p>
+                )}
+                {activo.ticket && (
+                  <p className="text-gray-700">N° TICKET: {activo.ticket}</p>
+                )}
               </div>
               <div>
                 <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded mr-2">
@@ -36,8 +79,8 @@ const ActivoList: React.FC<ActivoListProps> = ({ activos, setActivos }) => {
                   Editar
                 </button>
                 <button 
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
-                onClick={() => handleEliminar(indice)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-2"
+                  onClick={() => handleEliminar(indice)}
                 >
                   <Trash className="w-4 h-4 mr-2" />
                   Eliminar
@@ -45,7 +88,7 @@ const ActivoList: React.FC<ActivoListProps> = ({ activos, setActivos }) => {
                 {!activo.fechaSalida && (
                   <button
                     className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={() => handleAgregarSalida(activos, indice, '2024-01-01', '12345', 'Juan Pérez')}
+                    onClick={() => handleAgregarSalida(indice)}
                   >
                     <Plus className="w-4 h-4 mr-2" />
                     Agregar Salida
@@ -56,6 +99,59 @@ const ActivoList: React.FC<ActivoListProps> = ({ activos, setActivos }) => {
           </li>
         ))}
       </ul>
+
+      {/* Modal para agregar salida */}
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h2 className="modal-title">Agregar Salida</h2>
+            <div className="form-group">
+              <label htmlFor="asignadoA" className="form-label">Asignado a:</label>
+              <input
+                id="asignadoA"
+                type="text"
+                className="form-input"
+                value={formData.asignadoA}
+                onChange={(e) => setFormData({ ...formData, asignadoA: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="fechaSalida" className="form-label">Fecha de Salida:</label>
+              <input
+                id="fechaSalida"
+                type="date"
+                className="form-input"
+                value={formData.fechaSalida}
+                onChange={(e) => setFormData({ ...formData, fechaSalida: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="ticket" className="form-label">Ticket:</label>
+              <input
+                id="ticket"
+                type="text"
+                className="form-input"
+                value={formData.ticket}
+                readOnly
+              />
+            </div>
+            <div className="modal-actions">
+              <button
+                onClick={handleSubmit}
+                className="btn-save"
+              >
+                Guardar
+              </button>
+              <button
+                onClick={closeModal}
+                className="btn-cancel"
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
